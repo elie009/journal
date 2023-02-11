2b175ef555e45c6855dcf06382e9c79eb851ccce
elie <elibarabarquez.dev@mail.com>
2023-02-11 09:14:29 +0800

    successfully connected to MSSQL

--- Journal/Journal/Global.asax.cs
+++ Journal/Journal/Global.asax.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +18,32 @@ namespace Journal
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            string conStr = ConfigurationManager.ConnectionStrings["ConString"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(conStr))
+            {
+                try {
+                    conn.Open(); // throws if invalid
+                    System.Diagnostics.Debug.WriteLine("DB connection is success");
+                }
+                catch (SqlException sqe)
+                {
+                    System.Diagnostics.Debug.WriteLine("DB connection is invalid: ", sqe.Message);
+                }
+                finally
+                {
+                    try
+                    {
+                        conn.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        System.Diagnostics.Debug.WriteLine("DB connection error message: ", ex.Message);
+                    }
+                }
+            }
+            
+           
         }
     }
 }
