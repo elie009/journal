44d90e5724431426fa7623f40c07e102f056d2bc
elie <elibarabarquez.dev@mail.com>
2023-02-15 23:54:00 +0800

    add partial view files

--- Journal/Journal/App_Start/BundleConfig.cs
+++ Journal/Journal/App_Start/BundleConfig.cs
@@ -8,23 +8,58 @@ namespace Journal
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
 
+
+
+            bundles.Add(new Bundle("~/bundles/chart").Include(
+                        "~/Scripts/lib/chart/chart.min.js"
+                        ));
+            bundles.Add(new ScriptBundle("~/bundles/easing").Include(
+                        "~/Scripts/lib/easing/easing.js",
+                        "~/Scripts/lib/easing/easing.min.js"
+                        ));
+       
+            bundles.Add(new ScriptBundle("~/bundles/owlcarousel").Include(
+                        "~/Scripts/lib/owlcarousel/owl.carousel.js",
+                        "~/Scripts/lib/owlcarousel/owl.carousel.min.js"
+                        ));
+            bundles.Add(new ScriptBundle("~/bundles/tempusdominus").Include(
+                        "~/Scripts/lib/tempusdominus/moment-timezone.min.js",
+                        "~/Scripts/lib/tempusdominus/moment.min.js",
+                        "~/Scripts/lib/tempusdominus/tempusdominus-bootstrap-4.js",
+                        "~/Scripts/lib/tempusdominus/tempusdominus-bootstrap-4.min.js"
+                        ));
+            bundles.Add(new ScriptBundle("~/bundles/waypoints").Include(
+                        "~/Scripts/lib/waypoints/waypoints.min.js"
+                        ));
+
+            bundles.Add(new ScriptBundle("~/bundles/script").Include(
+                        "~/Scripts/main.js"
+                        ));
+
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap-lumen.css",
+            //          "~/Content/site.css"));
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/style.css"
+                      ));
+
+            bundles.Add(new StyleBundle("~/css/owlcarousel").Include(
+                      "~/Script/lib/owlcarousel/assets/owl.carousel.min.css"
+                      ));
+            bundles.Add(new StyleBundle("~/css/tempusdominus").Include(
+                      "~/Script/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css"
+                      ));
         }
     }
 }
--- Journal/Journal/Global.asax.cs
+++ Journal/Journal/Global.asax.cs
@@ -19,29 +19,6 @@ namespace Journal
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
-            string conStr = ConfigurationManager.ConnectionStrings["ConString"].ConnectionString;
-            using (SqlConnection conn = new SqlConnection(conStr))
-            {
-                try {
-                    conn.Open(); // throws if invalid
-                    System.Diagnostics.Debug.WriteLine("DB connection is success");
-                }
-                catch (SqlException sqe)
-                {
-                    System.Diagnostics.Debug.WriteLine("DB connection is invalid: ", sqe.Message);
-                }
-                finally
-                {
-                    try
-                    {
-                        conn.Close();
-                    }
-                    catch (Exception ex)
-                    {
-                        System.Diagnostics.Debug.WriteLine("DB connection error message: ", ex.Message);
-                    }
-                }
-            }
             
            
         }
